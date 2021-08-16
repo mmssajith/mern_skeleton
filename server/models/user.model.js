bb84e2fd38932bd66ba8c3cae0d39be8438aa31e
mmssajith <mmssajith@gmail.com>
2021-08-16 11:52:37 +0530

    Create Schema

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,5 +1,27 @@
 import mongoose from 'mongoose'
+import { string } from 'prop-types'
 
 const userSchema = new mongoose.Schema({
-    
+    name:{
+        type: string,
+        trim: true,
+        required: 'Name is required'
+    },
+    email:{
+        type: string,
+        trim: true,
+        unique: 'Email already exists',
+        match: [/.+\@.+\..+/,'Please enter a valid Email'],
+        required: 'Email is required'
+    },
+    created:{
+        type: Date,
+        default: Date.now
+    },
+    updated: Date,
+    hashed_password:{
+        type: string,
+        required: "Password is required"
+    },
+    salt: string
 })
